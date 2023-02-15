2be259ab6f5dd058dac5603377722dd5e7efa564
Rodrigo Góis <rodrigo.gois@questores.com.br>
2023-02-15 14:52:08 -0300

    Creating Form for Creating Courses

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-form',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
 
   }
 
