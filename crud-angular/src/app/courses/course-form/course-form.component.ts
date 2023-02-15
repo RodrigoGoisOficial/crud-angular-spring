b8f12e16c12ff445893872d0e2b6d769393554ad
Rodrigo Góis <rodrigo.gois@questores.com.br>
2023-02-15 16:27:18 -0300

    Angular Typed Forms

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from './../services/courses.service';
@@ -13,20 +13,17 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CourseFormComponent implements OnInit {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: [''],
+    category: ['']
+  });
 
   constructor(
     private coursesService: CoursesService,
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private snackBar: MatSnackBar,
     private location: Location
-  ) {
-    this.form = this.formBuilder.group({
-      id: '',
-      name: [null],
-      category: [null]
-    });
-  }
+  ) { }
 
   ngOnInit(): void {
 
